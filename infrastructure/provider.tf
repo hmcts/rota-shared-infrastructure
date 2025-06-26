82a588aaa312d90095a0f570abd39846871b9c15
Ben Neill <benjaminrneill@gmail.com>
2025-06-26 13:01:35 +0100

    provider config and tf env

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -8,3 +8,25 @@ provider "azurerm" {
   alias                      = "postgres_network"
   subscription_id            = var.aks_subscription_id
 }
+
+terraform {
+  backend "azurerm" {}
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.34"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "3.4.0"
+    }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.4.0"
+    }
+  }
+}
