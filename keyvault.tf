9ebf3383ec8a75a31567520018baa46dc225d40c
Ben Neill <benjaminrneill@gmail.com>
2025-06-26 15:33:29 +0100

    add in keyvault

---
+++ keyvault.tf
@@ -0,0 +1,10 @@
+module "key_vault" {
+  source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                = "${var.product}-${var.env}"
+  product             = var.product
+  env                 = var.env
+  object_id           = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
+  product_group_name  = "CPP Rota"
+  common_tags         = var.common_tags
+}
