fe60897807b0e099db6075068724d452a49950af
Ben Neill <benjaminrneill@gmail.com>
2025-07-01 08:42:59 +0100

    Update postgres.tf

--- postgres.tf
+++ postgres.tf
@@ -7,6 +7,7 @@ module "postgresql" {
   source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
   env    = var.env
 
+  name          = "rota-psql"
   product       = var.product
   component     = var.component
   resource_group_name = azurerm_resource_group.rg.name
