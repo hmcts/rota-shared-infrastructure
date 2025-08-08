0479c46ed2400f9a4eaf75b1cfdd8266d9b8a9c3
52960 <60659747+hmcts-jenkins-cnp[bot]@users.noreply.github.com>
2025-08-08 08:00:26 +0000

    Updating Terraform Formatting

--- postgres.tf
+++ postgres.tf
@@ -3,29 +3,29 @@ module "postgresql" {
   providers = {
     azurerm.postgres_network = azurerm.postgres_network
   }
-  
+
   source = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
   env    = var.env
 
-  name          = "rota-psql"
-  product       = var.product
-  component     = var.component
+  name                = "rota-psql"
+  product             = var.product
+  component           = var.component
   resource_group_name = azurerm_resource_group.rg.name
-  business_area = "sds" # sds or cft
-  subnet_suffix = "expanded"
+  business_area       = "sds" # sds or cft
+  subnet_suffix       = "expanded"
 
   pgsql_databases = [
     {
       name : "rota"
     }
   ]
-  pgsql_sku     = "GP_Standard_D2ds_v4"
-  pgsql_version = "16"
+  pgsql_sku             = "GP_Standard_D2ds_v4"
+  pgsql_version         = "16"
   geo_redundant_backups = var.postgres_geo_redundant_backups
 
   trigger_password_reset = "any value here"
-  
+
   admin_user_object_id = var.jenkins_AAD_objectId
-  
+
   common_tags = var.common_tags
 }
