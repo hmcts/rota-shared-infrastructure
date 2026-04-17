5e11b2bf20da6a69b62b960b0e32689237d1e038
Ben Neill <benjamin.neill@hmcts.net>
2026-04-17 12:25:11 +0000

    Write shared infrastructure secrets to Key Vault

---
+++ keyvault-secrets.tf
@@ -0,0 +1,39 @@
+resource "azurerm_key_vault_secret" "application_insights_connection_string" {
+  name         = "application-insights-connection-string"
+  value        = module.application_insights.connection_string
+  key_vault_id = module.key_vault.key_vault_id
+
+  tags = var.common_tags
+}
+
+resource "azurerm_key_vault_secret" "application_insights_instrumentation_key" {
+  name         = "application-insights-instrumentation-key"
+  value        = module.application_insights.instrumentation_key
+  key_vault_id = module.key_vault.key_vault_id
+
+  tags = var.common_tags
+}
+
+resource "azurerm_key_vault_secret" "postgresql_admin_username" {
+  name         = "postgresql-admin-username"
+  value        = module.postgresql.username
+  key_vault_id = module.key_vault.key_vault_id
+
+  tags = var.common_tags
+}
+
+resource "azurerm_key_vault_secret" "postgresql_admin_password" {
+  name         = "postgresql-admin-password"
+  value        = module.postgresql.password
+  key_vault_id = module.key_vault.key_vault_id
+
+  tags = var.common_tags
+}
+
+resource "azurerm_key_vault_secret" "postgresql_fqdn" {
+  name         = "postgresql-fqdn"
+  value        = module.postgresql.fqdn
+  key_vault_id = module.key_vault.key_vault_id
+
+  tags = var.common_tags
+}
