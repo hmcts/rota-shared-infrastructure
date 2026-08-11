927c61976e16e0d79b603634d37f5e1db8dc3569
Giles Anderson <giles.anderson@hmcts.net>
2026-08-11 21:10:39 +0100

    provision blob store

---
+++ storage.tf
@@ -0,0 +1,20 @@
+module "blobstore" {
+  source = "git@github.com:hmcts/cnp-module-storage-account?ref=4.x"
+
+  env                      = var.env
+  storage_account_name     = substr(lower(replace("${var.product}shared${var.env}", "-", "")), 0, 24)
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = var.postgres_geo_redundant_backups ? "GRS" : "LRS"
+
+  containers = [
+    {
+      name        = "anonymised-db-dumps"
+      access_type = "private"
+    }
+  ]
+
+  common_tags = local.merged_common_tags
+}
