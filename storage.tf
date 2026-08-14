2944443aa69db9307edfcfad806f20f63e0bedc9
Giles Anderson <giles.anderson@hmcts.net>
2026-08-14 14:21:55 +0100

    change blob store name format

--- storage.tf
+++ storage.tf
@@ -2,7 +2,7 @@ module "blobstore" {
   source = "git@github.com:hmcts/cnp-module-storage-account?ref=4.x"
 
   env                      = var.env
-  storage_account_name     = substr(lower(replace("${var.product}shared${var.env}", "-", "")), 0, 24)
+  storage_account_name     = substr(lower(replace("${var.product}-sa-${var.env}", "-", "")), 0, 24)
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_kind             = "StorageV2"
